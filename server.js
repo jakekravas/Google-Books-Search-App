1e86231d0e31e8a515c727c7d255e8da376bf49c
Jake Kravas <jakekravas7@gmail.com>
2020-04-14 18:14:01 -0700

    implemented express and mongoDB with react

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+
+const books = require("./routes/api/books");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+).then(() => console.log("MongoDB connected"));
+
+// Use Routes
+app.use("/api/books", books);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+})
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import './bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <button className="btn btn-primary">asdf</button>
     </div>
   );
 }
